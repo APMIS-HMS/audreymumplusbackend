e2db3d0ee2e6503689ac697b0e89d822a49451cb
lizzyladi <elizabeth.abbah@apmis.ng>
2018-06-13 12:54:04 +0100

    Fix mongosse url

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+const MONGODB_URI = 'mongodb://lizzy:lizzy081@ds159020.mlab.com:59020/audrey-mum';
+
 module.exports = function (app) {
-  mongoose.connect(app.get('mongodb'), {});
+  mongoose.connect(MONGODB_URI);
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
