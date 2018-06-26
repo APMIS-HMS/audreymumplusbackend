4e0180fef01e5ad9303ee8b117a0d49dd2e4eb27
lizzyladi <elizabeth.abbah@apmis.ng>
2018-06-26 16:09:00 +0100

    callback added to channels

--- src/channels.js
+++ src/channels.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   app.on('connection', connection => {
     // On a new real-time connection, add it to the anonymous channel
 
-    console.log('connection\n ====',connection.provider);
+    //console.log('connection\n ====',connection.provider);
     app.channel('anonymous').join(connection);
   });
 
