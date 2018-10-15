1e78b55d7442f088eb650a5ab4c6a40e303a161c
unknown <thadeus.ajayi@apmis.ng>
2018-10-15 16:33:56 +0100

    Add approved and forumMemberCount to forum model

--- src/models/forum.model.js
+++ src/models/forum.model.js
@@ -6,7 +6,9 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const forum = new Schema({
-    name:{type: String, unique:true, required:true }
+    name:{type: String, unique:true, required:true },
+    approved: {type: Boolean, Default: false},
+    forumMemberCount: {type: Number, Default: 0}
   }, {
     timestamps: true
   });
